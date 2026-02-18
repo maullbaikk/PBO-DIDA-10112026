b3056649eed11ce0bce489cc48d03bfaa20f7cf0
maullbaikk <didarifki792@gmail.com>
2026-02-18 12:55:54 +0700

    Add files via upload

---
+++ form_produk.php
@@ -0,0 +1,34 @@
+<html>
+    <head>
+    <title>
+        Belajar OOP - Form Produk
+    </title>
+
+
+
+
+    </head>
+
+    <body>
+        <h2>
+            INPUT DATA PRODUK 
+        </h2>
+        <form action="proses_produk.php" method="POST">
+            Nama Produk:
+            <input type="text" name="nama"> <br>
+<br>
+            Harga :
+            <input type="number" name="harga">
+
+            <input type="submit" name="kirim">
+
+
+
+        </form>
+
+    </body>
+
+
+
+
+</html>
